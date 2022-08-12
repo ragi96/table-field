64cda7b7ca31b9ce5a3d04867b12aca4c109dfe1
Martin Zurowietz <martin@zurowietz.de>
2022-08-12 19:29:45 +0200

    Fix handling of special strings
    This fixes the following cases:
    
    1. A string such as "This - That" was split in the middle although
    it should remain intact.
    
    2. Single quotes such as 'This - That' were not removed although
    they are used by the YAML parser to quote strings that include
    special characters.

--- src/components/table.vue
+++ src/components/table.vue
@@ -83,7 +83,10 @@
               }
               row = [];
             } else {
-              row.push(val.split('- ').pop().replace(/\""/g, ""));
+              let sanitizedVal = val.trim()
+                .replace(/^- /, "")
+                .replace(/^[\"\']|[\"\']$/g, "");
+              row.push(sanitizedVal);
             }
           });
           selfmadeValue.push(row);
